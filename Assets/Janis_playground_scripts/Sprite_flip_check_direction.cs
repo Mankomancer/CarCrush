e0e25fb4315828a74db959271af869c64ef4c3e1
Umplumps <moessync@gmail.com>
2023-10-10 22:01:14 +0300

    dir detect sprite flip

---
+++ Assets/Janis_playground_scripts/Sprite_flip_check_direction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_flip_check_direction : MonoBehaviour
+{
+    private Transform playerTransform;
+    private float playerPreviousPositionX;
+    private float playerCurrentPositionX;
+    private float x_scale_positive;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        x_scale_positive = playerTransform.localScale.x;
+        playerPreviousPositionX = playerTransform.position.x;
+        playerCurrentPositionX = playerTransform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCurrentPositionX = playerTransform.position.x;
+        if (playerCurrentPositionX > playerPreviousPositionX)
+        {
+            playerTransform.localScale = new Vector3(x_scale_positive, playerTransform.localScale.y, playerTransform.localScale.z);
+            playerPreviousPositionX = playerCurrentPositionX;
+        }
+        else if (playerCurrentPositionX < playerPreviousPositionX)
+        {
+            playerTransform.localScale = new Vector3(-x_scale_positive, playerTransform.localScale.y, playerTransform.localScale.z);
+            playerPreviousPositionX = playerCurrentPositionX;
+        }
+    }
+}
