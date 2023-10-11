7c140c7d0ede1765770f388b1355eab660c2edcc
Umplumps <moessync@gmail.com>
2023-10-11 22:17:31 +0300

    Start Scene UI

---
+++ Assets/Janis_playground_scripts/Start_Quit_button.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Quit_button : MonoBehaviour
+{
+    public void loadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
